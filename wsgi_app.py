a1f0f042857030fb1b85ea6d434a8cdaef2eb05a
Emanuel Cancino <ecancinoh@users.noreply.github.com>
2026-03-02 19:52:15 -0300

    chore: add wsgi_app.py as alternative startup file

---
+++ wsgi_app.py
@@ -0,0 +1,6 @@
+import sys
+import os
+sys.path.insert(0, '/home/padacicl/repositories/padaci')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'padaci.settings'
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
