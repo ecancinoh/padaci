9b846dd07282b8834f489930ff7c8bcad95da7db
Emanuel Cancino <ecancinoh@users.noreply.github.com>
2026-03-02 17:59:13 -0300

    fix: update paths to /home/padacicl/repositories/padaci

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.insert(0, '/home/padacicl/padaci')
+sys.path.insert(0, '/home/padacicl/repositories/padaci')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'padaci.settings'
 
 from django.core.wsgi import get_wsgi_application
